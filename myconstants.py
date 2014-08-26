b13027a949480910e6d7188b6dd74efc8f03707f
victorlslade <victorlslade@gmail.com>
2014-08-26 12:47:46 -0400

    started myconstants.py

---
+++ myconstants.py
@@ -0,0 +1,9 @@
+#Color Constants
+RED = (255, 0, 0)
+GREEN = (0, 255, 0)
+BLUE = (0, 0, 255)
+YELLOW = (255, 255, 0)
+MAGENTA = (255, 0, 255)
+CYAN = (0, 255, 255)
+WHITE =(255, 255, 255)
+BLACK = (0,0,0)
