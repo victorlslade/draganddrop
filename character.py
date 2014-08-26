fd6e126ea6a35e2d83fb10776399f6dcf4639ccd
victorlslade <victorlslade@gmail.com>
2014-08-26 10:23:28 -0400

    seperated class Character into character.py

---
+++ character.py
@@ -0,0 +1,13 @@
+
+import pygame as pg #lazy but responsible (avoid namespace flooding)
+
+class Character:
+    def __init__(self,rect):
+        self.rect = pg.Rect(rect)
+        self.click = False
+        self.image = pg.Surface(self.rect.size).convert()
+        self.image.fill((255,0,0))
+    def update(self,surface):
+        if self.click:
+            self.rect.center = pg.mouse.get_pos()
+        surface.blit(self.image,self.rect)
