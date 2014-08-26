b218477dc29eac0613236d04b3f065aebfd24426
victorlslade <victorlslade@gmail.com>
2014-08-26 13:00:30 -0400

    cleaned up some whitespace.  Reduced size of pygame window.

--- dd1.py
+++ dd1.py
@@ -1,11 +1,11 @@
-import os,sys
+import os, sys
 import pygame as pg #lazy but responsible (avoid namespace flooding)
 # character.py in this directory? a module?
 import character
 
 
 
-def main(Surface,Player):
+def main(Surface, Player):
     game_event_loop(Player)
     Surface.fill(0)
     Player.update(Surface)
@@ -22,13 +22,12 @@ def game_event_loop(Player):
 if __name__ == "__main__":
     os.environ['SDL_VIDEO_CENTERED'] = '1'
     pg.init()
-    Screen = pg.display.set_mode((1000,600))
+    Screen = pg.display.set_mode((640, 480))
     MyClock = pg.time.Clock()
-    MyPlayer =character.Character((0,0,150,30))
-    MyIcon = character.Character((300,0,150,30))
+    MyPlayer = character.Character((0, 0, 150, 30))
 
     #MyPlayer.rect.center = Screen.get_rect().center
     while 1:
-        main(Screen,MyPlayer)
+        main(Screen, MyPlayer)
         pg.display.update()
         MyClock.tick(60)
